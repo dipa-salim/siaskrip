59f4289f092379f7703e03935cf4e4da7a9eaa29
dandy <dandy.maulana99@gmail.com>
2022-01-19 11:15:02 +0700

    mod: add jwt to auth
    - new function loginjwt to allow generate token after login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,12 @@ use Session;
 use App\Models\User;
 use GuzzleHttp\RetryMiddleware;
 use Illuminate\Support\Facades\Auth;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use JWTFactory;
+use Illuminate\Support\Facades\Cookie;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 
 class AuthController extends Controller
@@ -44,13 +50,69 @@ class AuthController extends Controller
         return redirect("login");
     }
 
+    public function loginjwt(Request $request) {
+        $messages = [
+            'required' => ':attribute wajib diisi cuy!!!',
+            'email' => ':attribute wajib diisi sama email ya cuy!!!',
+        ];
+
+        $request->validate([
+            'email' => 'email',
+            'password' => 'required',
+        ],$messages);
+
+        $credentials = $request->only('email', 'password');
+        if ($token = Auth::attempt($credentials)) {
+            $customClaims = [
+                'email' => auth()->user()->email
+            ];
+
+            // $payload = JWTFactory::make($customClaims);
+            // $token = JWTAuth::encode($payload);
+            // dd($token);
+
+            // $cookie_token = cookie('smarttoken', $token, time() + 60 * 60 * 24, '/', '.app.test');
+            $data = [
+                'iat' => strtotime("now"),
+                'exp' => strtotime("now") + 72000,
+                'name' => Auth::user()->name,
+                'email' => Auth::user()->email,
+                'username' => Auth::user()->username,
+                'role' => Auth::user()->role,
+                'sub' => 'SSO-SMARTPTIK'
+            ];
+            $key = env('JWT_SECRET');
+            $jwt = JWT::encode(
+                $data,
+                $key,
+                'HS256'
+            );
+            $request->session()->put('username', Auth::user()->username);
+            $cookie_name = "smarttoken";
+            $cookie_value = $jwt;
+            setcookie($cookie_name, $cookie_value, strtotime("now") + (86400 * 1), "/", ".app.test");
+
+            // $cookie_token = cookie('smarttoken', $token, time() + 60 * 60 * 24, '/', '.app.test');
+            if (Auth::user()->role == "Admin") {
+                return redirect()->intended('/admin');
+            } else if (Auth::user()->role == "Dosen") {
+                return redirect()->intended('/dashboard-dosen');
+            } else if (Auth::user()->role == "Kaprodi") {
+                return redirect()->intended('/kaprodi');
+            } else {
+                return redirect()->intended('/');
+            }
+        }
+        return redirect("login");
+    }
+
     public function logout()
     {
         Session::flush();
-
+        $cookie = Cookie::forget('smarttoken');
         Auth::logout();
 
-        return redirect('login');
+        return redirect('login')->withCookie($cookie);
     }
 
     public function register()
